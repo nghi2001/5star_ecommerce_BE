b1d22b81279b0adb85de647775a55656c3494810
Nghi2001 <duynghikhum9@gmail.com>
2022-11-12 13:21:54 +0700

    UPDATE: comment model

--- src/entity/category.entity.ts
+++ src/entity/category.entity.ts
@@ -1,5 +1,5 @@
 import { Sub_Category } from 'src/entity/sub_category.entity'
-import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, CreateDateColumn, UpdateDateColumn, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
 import { Product } from './product.entity';
 
 @Entity()
@@ -16,10 +16,22 @@ export class Category extends BaseEntity {
     @Column()
     status: number;
 
+    @Column()
+    parent_id: number;
+
+    @ManyToOne(() => Category, (category) => category.sub_category)
+    @JoinColumn({ name: 'parent_id' })
+    parent_category: Category;
+
+    @OneToMany(() => Category, (sub_category) => sub_category.parent_category)
+    sub_category: Category[];
+
     @OneToMany(() => Sub_Category, (sub_category) => sub_category.category)
-    sub_categorys: Sub_Category[]
+    sub_categorys: Sub_Category[];
+
     @OneToMany(() => Product, (product) => product.category)
     products: Product[];
+
     @Column()
     @CreateDateColumn()
     create_at: Date;
